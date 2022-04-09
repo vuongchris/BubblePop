ff6a7dfe6924faa1c5db47f29bc91d567446fc19
Christopher Vuong <98904113+vuongchris@users.noreply.github.com>
2022-04-09 13:50:30 +1000

    Added Timer

--- Controller/GameViewController.swift
+++ Controller/GameViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var remainingTimeLabel: UILabel!
+    @IBOutlet weak var currentScoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
+    var remainingTime = 60
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        remainingTimeLabel.text = String(remainingTime)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
+            timer in
+            self.countingDown()
+        }
+        
     }
-
+    
+    @objc func countingDown() {
+        remainingTime -= 1
+        remainingTimeLabel.text = String(remainingTime)
+        
+        if remainingTime == 0 {
+            timer.invalidate()
+            print("Time is over!")
+        }
+    }
 
 }
